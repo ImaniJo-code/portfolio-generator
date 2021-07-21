f1b767645b237941b958f29b52ee7886786d08d9
ImaniJo-code <imani.l.johnson@gmail.com>
2021-07-20 21:15:35 -0400

    inquirer added

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
+const inquirer = require('inquirer');
+//const fs = require('fs');
+//const generatePage = require('./src/page-template.js');
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
+//const pageHTML = generatePage(name, github);
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//fs.writeFile('./index.html', pageHTML, err => {
+    //if (err) throw err;
 
-// const printProfileData = profileDataArr => {
-    // This...
-    // for (let i = 0; i < profileDataArr.length; i += 1) {
-        // console.log(profileDataArr[i]);
-    // }
+    //console.log('Portfolio complete! Check out index.html to see the output!');
+//});
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
 
-    // console.log('================');
-
-    // Is the same as this...
-    // profileDataArr.forEach((profileItem) => console.log(profileItem));
-// };
-
-// printProfileData(profileDataArgs);
